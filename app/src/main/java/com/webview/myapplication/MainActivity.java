bed72701c177b7dfd13c4ae4ebae2ecc7059b64d
awdevmyid <144774921+awdevmyid@users.noreply.github.com>
2023-09-24 10:14:03 +0700

    Update MainActivity.java

--- app/src/main/java/com/webview/myapplication/MainActivity.java
+++ app/src/main/java/com/webview/myapplication/MainActivity.java
@@ -65,7 +65,7 @@ public class MainActivity extends Activity {
             dm.enqueue(request);
             Toast.makeText(getApplicationContext(), "Downloading File", Toast.LENGTH_LONG).show();
         });
-        mWebView.loadUrl("https://github.com/satyakami"); //Replace The Link Here
+        mWebView.loadUrl("https://www.awdev.my.id"); //Replace The Link Here
     }
     private static class HelloWebViewClient extends WebViewClient
     {
